d1511534afba8d28c7195d36d1b3db81bc2c5880
efrenjr-dev <efrenjr.rodiguez@gmail.com>
2021-12-16 16:20:06 +0300

    updated about me section

--- src/components/About.js
+++ src/components/About.js
@@ -24,9 +24,9 @@ export default function About() {
               className="bg-white px-4 px-md-5 pt-4 pt-md-5 pb-5 justify-content-start shadow"
             >
               <h1>Efren Rodriguez</h1>
-              <h4 className="text-muted pb-3">Fullstack Developer </h4>
+              <h4 className="text-muted pb-3">Fullstack Developer (MERN)</h4>
               <h5>Email:</h5>
-              <p>efrenjr.rodriguezl@gmail.com</p>
+              <p>efrenjr.rodriguez@gmail.com</p>
               <h5>Location:</h5>
               <p>Riyadh, Saudi Arabia</p>
               <h5>Date of Birth:</h5>
